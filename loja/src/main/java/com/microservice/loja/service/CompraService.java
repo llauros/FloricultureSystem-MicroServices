478ad64376eb98ae593ea5b69319636c25fcffbe
llauros <lucaslauro@live.com>
2022-03-24 17:25:35 -0300

    Acessando serviço-fornecedor com restTemplate

--- loja/src/main/java/com/microservice/loja/service/CompraService.java
+++ loja/src/main/java/com/microservice/loja/service/CompraService.java
@@ -1,19 +1,24 @@
 package com.microservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.microservice.loja.dto.CompraDTO;
 import com.microservice.loja.dto.InfoFornecedorDTO;
 
+@Service
 public class CompraService {
 
+	@Autowired
+	private RestTemplate client;
+	
 	public void realizaCompra(CompraDTO compra) {
-		
-		RestTemplate client = new RestTemplate();
+
 		ResponseEntity<InfoFornecedorDTO> exchange;
-		exchange = client.exchange("http://localhost:8081/info/" + compra.getEndereco().getEstado(),
+		exchange = client.exchange("http://fornecedor/info/" + compra.getEndereco().getEstado(),
 				HttpMethod.GET, null, InfoFornecedorDTO.class);
 		
 		System.out.println(exchange.getBody().getEndereco());
