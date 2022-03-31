47a685ff6e8bfb7d3b4b800425dcd7bb36fc2657
llauros <lucaslauro@live.com>
2022-03-31 17:25:14 -0300

    Comunicação utilizando RestTemplate

--- loja/src/main/java/com/microservice/loja/service/CompraService.java
+++ loja/src/main/java/com/microservice/loja/service/CompraService.java
@@ -1,6 +1,7 @@
 package com.microservice.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,11 +16,19 @@ public class CompraService {
 	@Autowired
 	private RestTemplate client;
 	
+	@Autowired
+	private DiscoveryClient eurekaCLient;	
+	
 	public void realizaCompra(CompraDTO compra) {
 
 		ResponseEntity<InfoFornecedorDTO> exchange;
 		exchange = client.exchange("http://fornecedor/info/" + compra.getEndereco().getEstado(),
 				HttpMethod.GET, null, InfoFornecedorDTO.class);
+		
+		eurekaCLient.getInstances("fornecedor").stream()
+		.forEach(fornecedor -> {
+			System.out.println("fornecedor: " + fornecedor.getPort());
+		});
 		
 		System.out.println(exchange.getBody().getEndereco());
 	}
